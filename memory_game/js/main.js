236fe93c9e080ea632c6602138dec05030e418fe
Christine L <christinel@Christines-MacBook-Pro-2.local>
2019-01-29 22:14:43 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,6 +26,8 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
@@ -35,7 +37,9 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
 
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -46,5 +50,15 @@ var flipCard = function(cardId) {
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', cards[i]);
+
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
